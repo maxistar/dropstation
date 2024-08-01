e3699c7e3c6404ca34b5f269153007851f0b23cb
Max Starikov <mstarikov@itemis.com>
2024-08-01 09:49:47 +0200

    edit dialog

--- nuxt-crud-app/components/DeviceItem.vue
+++ nuxt-crud-app/components/DeviceItem.vue
@@ -8,27 +8,20 @@
       <v-btn @click="showConfirmDialog" color="error">Delete</v-btn>
     </v-card-actions>
 
-    <!-- Confirmation Dialog -->
-    <v-dialog v-model="dialog" max-width="500">
-      <v-card>
-        <v-card-title class="headline">Confirm Deletion</v-card-title>
-        <v-card-text>
-          Are you sure you want to delete this device?
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" text @click="dialog = false">Cancel</v-btn>
-          <v-btn color="error" text @click="confirmDelete">Delete</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <ConfirmationDialog
+        :isOpen="dialog"
+        title="Confirm Deletion"
+        message="Are you sure you want to delete this device?"
+        @update:isOpen="dialog = $event"
+        @confirm="confirmDelete"
+    />
   </v-card>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import { defineProps, defineEmits } from 'vue';
-import { VCard, VCardText, VCardActions, VBtn, VSpacer } from 'vuetify/components';
+import ConfirmationDialog from '~/components/ConfirmationDialog.vue';
 
 const props = defineProps({
   device: {
@@ -47,7 +40,6 @@ const showConfirmDialog = () => {
 
 const confirmDelete = () => {
   emit('delete', props.device.id);
-  dialog.value = false;
 };
 </script>
 
